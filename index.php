a44ac2223fafc2255227474974584ce5000c8b7a
Daniel Sánchez González <danielsanchezgonzalez@alumno.ieselrincon.es>
2023-09-29 10:29:36 +0100

    Pequeño cambio

--- index.php
+++ index.php
@@ -4,30 +4,23 @@ function sergio() {
     echo "Esta es mi función";
 }
 
-<<<<<<< HEAD
+
 function enrique(){
     echo "saludos";
 }
 
-=======
 
 function daniel() {
     echo "Esto es una prueba";
 }
->>>>>>> 93780c50d12759c47829a034c31e7a85d33ad54f
+
 function Anahel() {
     echo "Esta es mi función";
-
 }
 
 function cristoRubenPerezSuarez() {
     echo "Soy una función";
 }
 
-<<<<<<< HEAD
-?>
-
-=======
 
 ?>
->>>>>>> 93780c50d12759c47829a034c31e7a85d33ad54f
